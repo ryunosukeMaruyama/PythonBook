fdb780307b845c8110cb324d13c05d7262ef4928
rmar001 <rmar001@gmail.com>
2020-01-20 08:02:55 +0900

    chap03-008完了

--- work/Python/03/03-009.py
+++ work/Python/03/03-009.py
@@ -1,4 +1,10 @@
 # %%
+import re
+import json
+import requests
+import random
+from sklearn.metrics import mean_absolute_error
+from sklearn.model_selection import train_test_split
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -64,9 +70,9 @@ print(lr_multi.coef_)
 print(lr_multi.intercept_)
 
 # %%
-from sklearn.model_selection import train_test_split
 
-X_train, X_test, Y_train, Y_test = train_test_split(data_boston_x, data_boston_y, test_size=0.3, random_state=123)
+X_train, X_test, Y_train, Y_test = train_test_split(
+    data_boston_x, data_boston_y, test_size=0.3, random_state=123)
 
 # %%
 print(X_train.shape)
@@ -89,7 +95,6 @@ print(y_pred - Y_test)
 
 
 # %%
-from sklearn.metrics import mean_absolute_error
 
 x_column_list = ['RM']
 y_column_list = ['PRICE']
@@ -121,16 +126,7 @@ y_pred = lr_multi2.predict(X_test)
 print(mean_absolute_error(y_pred, y_test))
 
 # %%
-import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
-import random
-from sklearn.linear_model import LinearRegression
-from sklearn.model_selection import train_test_split
 
-import requests
-import json
-import re
 
 url_path = 'https://www.land.mlit.go.jp/webland/api/TradeListSearch?from=20142&to=20153&area=10'
 request_result = requests.get(url_path)
@@ -189,5 +185,151 @@ print(data_selected.shape)
 data_selected_dropna = data_selected.dropna(how='any')
 print(data_selected_dropna.shape)
 data_selected_dropna.iloc[0]
+
+# %%
+data_selected_dropna['建築年'].unique()
+
+# %%
+data_selected_dropna = data_selected_dropna[data_selected_dropna['建築年'].str.match(
+    '^平成|昭和')]
+wareki_to_seireki = {'昭和': 1926-1, '平成': 1989-1}
+
+building_year_list = data_selected_dropna['建築年']
+
+building_age_list = []
+for building_year in building_year_list:
+    building_year_split = re.search(r'(.+?)([0-9]+|元)年', building_year)
+    seireki = wareki_to_seireki[building_year_split.groups()[0]] + \
+        int(building_year_split.groups()[1])
+    building_age = 2019 - seireki
+    building_age_list.append(building_age)
+
+data_selected_dropna['築年数'] = building_age_list
+data_added_building_age = data_selected_dropna.drop('建築年', axis=1)
+data_added_building_age.head()
+
+# %%
+# ダミー変数化しないものリスト
+columns_name_list = [
+    '最寄駅：距離（分）',
+    '面積（㎡）',
+    '築年数',
+    '建ぺい率（％）',
+    '容積率（％）',
+    '取引価格（総額）'
+]
+
+# ダミー変数リスト
+dummy_list = [
+    '間取り',
+    '建物の構造',
+    '市区町村名'
+]
+
+# ダミー変数を追加
+data_added_dummies = pd.concat(
+    [
+        data_added_building_age[columns_name_list],
+        pd.get_dummies(data_added_building_age[dummy_list],
+                       drop_first=True)
+    ],
+    axis=1
+)
+
+# %%
+print(data_added_dummies.dtypes)
+
+# %%
+data_added_dummies['面積（㎡）'] = data_added_dummies['面積（㎡）'].astype(float)
+data_added_dummies = data_added_dummies[~data_added_dummies['最寄駅：距離（分）'].str.contains('\?')]
+data_added_dummies['最寄駅：距離（分）'] = data_added_dummies['最寄駅：距離（分）'].astype(float)
+
+# %%
+plt.hist(data_added_dummies['取引価格（総額）'])
+plt.show()
+
+# %%
+tmp_data = data_added_dummies[data_added_dummies['取引価格（総額）'] < 60000000]
+print(tmp_data.shape)
+plt.hist(tmp_data['取引価格（総額）'])
+plt.show
+
+# %%
+data_added_dummies = data_added_dummies[data_added_dummies['取引価格（総額）'] < 60000000]
+
+# %%
+lr = LinearRegression()
+
+x_column_list = ['面積（㎡）']
+y_column_list = ['取引価格（総額）']
+
+x = data_added_dummies[x_column_list]
+y = data_added_dummies[y_column_list]
+
+lr.fit(x, y)
+
+print(lr.coef_)
+print(lr.intercept_)
+
+# %%
+x = data_added_dummies.drop('取引価格（総額）', axis=1)
+y = data_added_dummies['取引価格（総額）']
+
+print(x.head())
+print(y.head())
+
+# %%
+lr_multi = LinearRegression()
+lr_multi = lr_multi.fit(x, y)
+
+print(lr_multi.coef_)
+print(lr_multi.intercept_)
+
+# %%
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.3)
+print(X_train.shape)
+print(X_test.shape)
+print(y_train.shape)
+print(y_test.shape)
+
+# %%
+lr_multi2 = LinearRegression()
+lr_multi2.fit(X_train, y_train)
+print(lr_multi2.coef_)
+print(lr_multi2.intercept_)
+
+# %%
+y_pred = lr_multi2.predict(X_test)
+print(y_pred - y_test)
+
+# %%
+from sklearn.metrics import mean_absolute_error
+
+x_column_list = ['面積（㎡）']
+y_column_list = ['取引価格（総額）']
+
+X_train, X_test, y_train, y_test = train_test_split(
+    data_added_dummies[x_column_list],
+    data_added_dummies[y_column_list],
+    test_size=0.3
+)
+
+lr_single = LinearRegression()
+lr_single.fit(X_train, y_train)
+y_pred = lr_single.predict(X_test)
+
+mean_absolute_error(y_pred, y_test)
+
+# %%
+x = data_added_dummies.drop('取引価格（総額）', axis=1)
+y = data_added_dummies['取引価格（総額）']
+
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.3)
+
+lr_multi2 = LinearRegression()
+lr_multi2.fit(X_train, y_train)
+y_pred = lr_multi2.predict(X_test)
+
+mean_absolute_error(y_pred, y_test)
 
 # %%
